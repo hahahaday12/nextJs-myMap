aa9a5625bc98db5559a3c338a62ec9bb55c46137
hahahaday12 <hahahaday12@naver.com>
2023-09-21 00:10:19 +0900

    Init: 프로젝트 초기 세팅

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [''], // 외부에서 오는 이미지 도메인을 허용하기 위한 설정. 재시작 필요
+  },
+};
+
+module.exports = nextConfig;

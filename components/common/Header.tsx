ffc2c5849a8fa9e084f0c7e1985a8d048a5000c5
hahahaday12 <hahahaday12@naver.com>
2023-10-12 23:43:54 +0900

    Feat: 공통 header 컴포넌트 생성

---
+++ components/common/Header.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../../styles/header.module.scss';
+
+interface Props {
+  onClickLogo?: () => void;
+  rightElements?: React.ReactElement[];
+}
+
+const HeaderComponent = ({ onClickLogo, rightElements }: Props) => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.flexItem}>
+        <Link
+          href="/"
+          onClick={onClickLogo}
+          className={styles.box}
+          aria-label="홈으로 이동"
+        >
+          {/*  외부 링크로 이미지를 넣을땐 넓이와, 높이 꼭 지정해줘야함 이미지 크기 사이즈 모를떈 fill을 이용*/}
+          {/* 납짝한 사진을 방지 하게 위해 인라인 스타일 objectFit: 'cover */}
+          <Image src="" width={110} height={20} alt="로고" priority />
+        </Link>
+      </div>
+      {rightElements && <div className={styles.flexItem}>{rightElements}</div>}
+    </header>
+  );
+};
+
+export default HeaderComponent;

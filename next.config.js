29a852da3804b5863a1c6d0cb259835971ffab56
hahahaday12 <hahahaday12@naver.com>
2023-10-12 23:48:18 +0900

    Chore: StrictMode 세팅 변경

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: [''], // 외부에서 오는 이미지 도메인을 허용하기 위한 설정. 재시작 필요
   },

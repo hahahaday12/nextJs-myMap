334991afcdf8f011b31de01fd64c38ada27506d1
hahahaday12 <hahahaday12@naver.com>
2023-10-16 00:19:58 +0900

    Feat: 피드백 페이지

---
+++ pages/feedback.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/common/Header';
+import styles from '../styles/header.module.scss';
+import Link from 'next/link';
+
+// 피드백 페이지 
+export default function Feedback() {
+  return (
+    <>
+      <Header />
+      <main></main>
+    </>
+  );
+}

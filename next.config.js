de1d4b75c3a73c6e1bc61227c1c5f36a95692d8f
hahahaday12 <hahahaday12@naver.com>
2023-10-16 00:03:16 +0900

    Feat: stricMode 변경

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   images: {
     domains: [''], // 외부에서 오는 이미지 도메인을 허용하기 위한 설정. 재시작 필요
   },

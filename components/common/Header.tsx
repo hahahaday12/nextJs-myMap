c74ed94b9dd7f1de277a6bad0a9fe62b2a8f2084
hahahaday12 <hahahaday12@naver.com>
2023-10-16 00:19:44 +0900

    Feat: logo 추가 생성

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -15,12 +15,19 @@ const HeaderComponent = ({ onClickLogo, rightElements }: Props) => {
         <Link
           href="/"
           onClick={onClickLogo}
-          className={styles.box}
+          className={styles.linkbox}
           aria-label="홈으로 이동"
         >
           {/*  외부 링크로 이미지를 넣을땐 넓이와, 높이 꼭 지정해줘야함 이미지 크기 사이즈 모를떈 fill을 이용*/}
           {/* 납짝한 사진을 방지 하게 위해 인라인 스타일 objectFit: 'cover */}
-          <Image src="" width={110} height={20} alt="로고" priority />
+          <Image
+            className={styles.imagebox}
+            src="/maplogo.png"
+            width={110}
+            height={20}
+            alt="맛남지도"
+            priority
+          ></Image>
         </Link>
       </div>
       {rightElements && <div className={styles.flexItem}>{rightElements}</div>}
